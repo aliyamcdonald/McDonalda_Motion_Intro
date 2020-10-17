2b69524450d131dcaf0c8ecc716780c8e6248f87
aliyamcdonald <63318900+aliyamcdonald@users.noreply.github.com>
2020-10-16 20:52:22 -0400

    starting from scratch

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,18 +1,3 @@
-// import * as Demo from './demo.js';
+import * as Demo from './demo.js';
 
-import {gsap} from "gsap";
-
-import {pinkBoxAnimation} from "./pinkBox.js"
-import {tealBoxAnimation} from "./tealBox.js"
-import {yellowBoxAnimation} from "./yellowBox.js"
-
-// console.log(Demo);
-
-const mainTL = gsap.timeline({paused:true});
-
-    mainTL.add(pinkBoxAnimation())
-        .add(tealBoxAnimation())
-        .add(yellowBoxAnimation())
-        .play();
-
-
+console.log(Demo);
