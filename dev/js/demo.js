a7029403e687e0e31190d56ca3358d7807ced3e8
aliyamcdonald <63318900+aliyamcdonald@users.noreply.github.com>
2020-10-19 15:50:26 -0400

    scale

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -11,6 +11,21 @@ const iconTL = gsap.timeline();
 iconTL.from("#center",{duration: 1, scale:0, alpha:0})
 iconTL.from("#petals",{duration: 2, scale:0, alpha:0})
 iconTL.from("#petals",{rotate: 360, duration: 2})
+iconTL.to("#petal1",{scale: 1.2})
+iconTL.to("#petal1",{scale: 1})
+iconTL.to("#petal2",{scale: 1.2})
+iconTL.to("#petal2",{scale: 1})
+iconTL.to("#petal3",{scale: 1.2})
+iconTL.to("#petal3",{scale: 1})
+iconTL.to("#petal4",{scale: 1.2})
+iconTL.to("#petal4",{scale: 1})
+iconTL.to("#petal5",{scale: 1.2})
+iconTL.to("#petal5",{scale: 1})
+iconTL.to("#petal6",{scale: 1.2})
+iconTL.to("#petal6",{scale: 1})
+iconTL.to("#center",{scale: 1.2})
+iconTL.to("#center",{scale: 1})
+
 
 
 
